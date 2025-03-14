9953bd53b7b52a9919cc118f658a21adf2072d52
Robert Cigan <cigan.robert@gmail.com>
2025-03-14 17:04:00 +0100

    oprava typu desky

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,15 @@
 HomeControl home_control;
 
 void setup() {
-  // Wire.begin();
-  // #if defined(WITH_SERIAL)
-  delay(5000);
-  Serial.begin(115200);
-  Serial.println(F("Home Control loading..."));
-  // #endif
-  // home_control.setup();
+  Wire.begin();
+  #if defined(WITH_SERIAL)
+    delay(5000);
+    Serial.begin(115200);
+    Serial.println(F("Home Control loading..."));
+  #endif
+  home_control.setup();
 }
 
 void loop() {
-  //home_control.loop();  
-  Serial.println("Ho");
-  delay(1000);
+  home_control.loop();
 }
